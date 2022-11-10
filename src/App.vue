147afde4f86f2db4e5c6b23da146c910cb881eee
Tvisher <tvisher@mail.ru>
2022-11-10 16:17:49 +0600

    fix zero price

--- src/App.vue
+++ src/App.vue
@@ -450,7 +450,7 @@ export default {
       if (
         !this.serviceTitle.trim() ||
         !this.serviceDescription.trim() ||
-        !this.servicePrice === ""
+        !this.servicePrice.trim() === ""
       ) {
         return;
       }
